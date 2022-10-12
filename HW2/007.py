3baf5dfa734f5aa18143b25dc44ed46c2bce6374
Andrei <apchilap@gmail.com>
2022-10-12 18:01:01 +0600

    few minor fixes

--- HW2/007.py
+++ HW2/007.py
@@ -4,13 +4,16 @@ import math
 
 number = int(input("Введите число N: "))
 
+
 def Sequence(n):
     lst = []
     sum = 0
     for i in range(0, n):
-        lst.append(((math.pow((1+(1/n)), n))))
+        lst.append(math.pow((1+(1/n)), n))
         sum = sum + lst[i]
     return (sum)
 
+
 result = round(Sequence(number))
-print(f"Сумма чисел последовательности (1+1/n)^n из {number} элементов = {result}")
+print(
+    f"Сумма чисел последовательности (1+1/n)^n из {number} элементов = {result}")
